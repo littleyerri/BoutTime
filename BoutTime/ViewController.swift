288f883d5c50b015c42419fa873562e5b361244a
Gerardo Zayas <jerry@zayas.com>
2017-07-04 22:18:07 -0400

    Added Outlets for all labels and buttons.

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOulets
+    
+    // UILabel
+    @IBOutlet weak var firstEvent:  UILabel!
+    @IBOutlet weak var secondEvent: UILabel!
+    @IBOutlet weak var thirdEvent:  UILabel!
+    @IBOutlet weak var fourthEvent: UILabel!
+    
+    @IBOutlet weak var countdown:   UILabel!
+    @IBOutlet weak var message:     UILabel!
+    
+    // UIButton
+    @IBOutlet weak var firstEventDown:  UIButton!
+    @IBOutlet weak var secondEventUp:   UIButton!
+    @IBOutlet weak var secondEventDown: UIButton!
+    @IBOutlet weak var thirdEventUp:    UIButton!
+    @IBOutlet weak var thirdEventDown:  UIButton!
+    @IBOutlet weak var fourthEventUp:   UIButton!
+    
+    @IBOutlet weak var nextRoundSucess: UIButton!
+    @IBOutlet weak var nextRoundFail:   UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
